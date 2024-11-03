ebe33517b46ba4827f7bd8d8f409bc5687b4023f
Ibrahim brohi <133601225+Meer786777@users.noreply.github.com>
2024-11-03 07:54:04 -0800

    okay

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,7 @@ import Text3 from './components/task2';
 function App() {
   return (
     <div className="App">
-              <div className="text-container">
-            <h1 className="text-title">Welcome to the Image Converter</h1>
-            </div>
-         <ImageConverter />
+        <ImageConverter />
         <Text1/>
         <Text3/>
     </div>
